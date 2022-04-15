a3fe105a860bea7b5117a70071307bea82579236
By_JumperX4 <private@b4.anonaddy.me>
2022-04-15 19:22:54 +0200

    Add accept headers properly

--- generate.sh
+++ generate.sh
@@ -26,9 +26,21 @@ sh $CURR_DIR/filters.sh $CURR_DIR/temp
 # Generate oscpu
 sh $CURR_DIR/oscpu.sh $CURR_DIR/temp
 
+# Accept headers, those are not generated
+
+echo "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+text/html,application/xhtml+xml,application/xml;q=0.9,image/jxl,image/webp,*/*;q=0.8" > $CURR_DIR/temp/accept-headers.txt
+
 # Move the final files out of the temp dir
 mv $CURR_DIR/temp/user-agents.txt $CURR_DIR/user-agents.txt
 mv $CURR_DIR/temp/oscpu.txt $CURR_DIR/oscpu.txt
+mv $CURR_DIR/temp/accept-headers.txt $CURR_DIR/accept-headers.txt
 
 # Remove temp dir as we don't need it anymore
 rm -rfv $CURR_DIR/temp
